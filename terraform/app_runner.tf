47df0a38c6290d34502bbe176add3c9f5a782077
Manuel Padial <manolo.padial.2001@gmail.com>
2025-04-30 14:12:56 +0200

    app_runner

--- terraform/app_runner.tf
+++ terraform/app_runner.tf
@@ -1,35 +1,5 @@
-# Repositorio ECR
-resource "aws_ecr_repository" "api_repository" {
-  name = "intento_terraform"
-}
-
-# Rol IAM para acceso a ECR desde App Runner
-resource "aws_iam_role" "apprunner_role" {
-  name = "apprunner-ecr-access-role"
-  
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "build.apprunner.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-# Adjuntar política de acceso a ECR específica para App Runner
-resource "aws_iam_role_policy_attachment" "apprunner_policy_attachment" {
-  role       = aws_iam_role.apprunner_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
-}
-
-# Servicio App Runner
 resource "aws_apprunner_service" "dash_app" {
-  service_name = "skew-volatilidad-app"
+  service_name = var.app_runner_service_name
 
   source_configuration {
     authentication_configuration {
@@ -40,7 +10,7 @@ resource "aws_apprunner_service" "dash_app" {
       image_configuration {
         port = "8050"
       }
-      image_identifier      = "${aws_ecr_repository.api_repository.repository_url}:latest"
+      image_identifier      = "${aws_ecr_repository.api_repository.repository_url}:${var.image_tag}"
       image_repository_type = "ECR"
     }
 
@@ -48,6 +18,7 @@ resource "aws_apprunner_service" "dash_app" {
   }
 
   tags = {
-    Name = "skew-volatilidad-app"
+    Name = var.app_runner_service_name
   }
 }
+
